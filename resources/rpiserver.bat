3a78710d944e38aad518a65e823644c7fb9e4c73
ciss84 <cj84_84120@outlook.com>
2020-06-10 22:03:57 +0200

    resources

---
+++ resources/rpiserver.bat
@@ -0,0 +1,7 @@
+@ECHO off
+ECHO Configure your server start process here.
+ECHO Or just delete the bat and start the server manually.
+
+REM miniweb.exe -p 8888 -r "C:\TEMP\"
+REM serve-win.exe -n -l 8888 "C:\TEMP\"
+pause
---
+++ resources/start.bat
@@ -0,0 +1,7 @@
+start resources\UwAmp.exe
+start resources\ps4serve.exe
+start resources\Ps4RemotePkgs.exe
+start resources\PS4RPI.exe
+start resources\NetCatGUI\NetCatGUI.exe
+start resources\FTPClient\filezilla.exe
+start resources\lanplayGUI\lanplayGUI.exe
